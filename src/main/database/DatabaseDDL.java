ca7f2008d87b74abdbfa5b3f29050af1d6f17334
artis <artis.brabencova@student.kdg.be>
2024-12-12 17:17:13 +0100

    feature: DDL test

---
+++ src/main/database/DatabaseDDL.java
@@ -0,0 +1,22 @@
+package main.database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseDDL {
+// TEST DATABASE CONNECTION
+    DatabaseConnection.getConnection();
+    Create create = new Create();
+    create.createPlayerTable();
+
+    //Create a statement
+    Statement statement = connection.createStatement();
+
+    //Creating a table
+    statement.executeUpdate()
+
+
+
+
+}
